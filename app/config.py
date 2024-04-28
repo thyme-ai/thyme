a01a354ea008f032cf6bda0ae4d89f920c160769
Frankie Willcox Esparza <frankie@madisons-mbp-2.lan>
2024-04-28 13:41:57 -0700

    update config file so sqlalchemy works with Heroku

--- app/config.py
+++ app/config.py
@@ -1,8 +1,13 @@
 from os import environ
 
 class Configuration:
-    SECRET_KEY = environ.get('SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI = environ.get('DATABASE_URL')
+    # Adjustments to Databsae URI required for Heroku to Work with SQLAlchemy
+    URI = environ.get('DATABASE_URL')
+    if URI.startswith("postgres://"):
+        URI = URI.replace("postgres://", "postgresql://", 1)
+    SQLALCHEMY_DATABASE_URI = URI
+    
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SECRET_KEY = environ.get('SECRET_KEY')
     GOOGLE_CLIENT_CONFIG= environ.get('GOOGLE_CLIENT_CONFIG')
     OAUTHLIB_INSECURE_TRANSPORT = environ.get('OAUTHLIB_INSECURE_TRANSPORT')
